c79fa2b339b700820b020b104a25a96c70cfe962
JadsonLucena <jadsonlucena@gmail.com>
2022-12-17 14:11:36 -0300

    feat (project repository): find

--- src/infra/gateways/repository/Project.ts
+++ src/infra/gateways/repository/Project.ts
@@ -30,4 +30,96 @@ export default class ProjectRepository extends Repository implements IProjectRep
 
 	}
 
+	async find({
+		id,
+		userId,
+		name,
+		createdBy,
+		createdAt
+	}:{
+		id?: string,
+		userId?: string,
+		name?: string,
+		createdBy?: string,
+		createdAt?: string
+	}, transactionId?: string): Promise<Project[]> {
+
+		let tables = [`projects`]
+		let where: string[] = []
+
+		let params: {
+			id?: string,
+			name?: string,
+			userId?: string,
+			createdBy?: string,
+			createdAt?: string
+		} = {}
+		let types: {
+			id?: 'string',
+			name?: 'string',
+			userId?: 'string',
+			createdBy?: 'string',
+			createdAt?: 'timestamp'
+		} = {}
+
+		if (id) {
+
+			where.push(`id = @id`)
+			params.id = new UUID(id).toString()
+			types.id = 'string'
+
+		} else {
+
+			if (userId) {
+
+				tables.push(`members`)
+				where.push(`projects.id = members.project_id`)
+				where.push(`members.user_id = @userId`)
+				params.userId = new UUID(userId).toString()
+				types.userId = 'string'
+
+			}
+
+			if (name) {
+
+				where.push(`projects.name = @name`)
+				params.name = new Name(name, {
+					minAmountOfLastNames: 0
+				}).toString()
+				types.name = 'string'
+
+			}
+
+			if (createdBy) {
+
+				where.push(`projects.created_by = @createdBy`)
+				params.createdBy = new UUID(createdBy).toString()
+				types.createdBy = 'string'
+
+			}
+
+			if (createdAt) {
+
+				where.push(`projects.created_at = @createdAt`)
+				params.createdAt = new Date(createdAt).toISOString()
+				types.createdAt = 'timestamp'
+
+			}
+
+		}
+
+		const [ rows, state, metadata ] = await (transactionId ? this.transactions[transactionId] : this.database).run({
+			sql: `SELECT * FROM ${tables.join(`, `)} ${where.length ? `WHERE ${where.join(` AND `)}` : ''}`,
+			params,
+			types
+		}).catch((err: any) => {
+
+			throw err
+
+		})
+
+		return rows.map((row: any) => new Project(row.toJSON()))
+
+	}
+
 }
