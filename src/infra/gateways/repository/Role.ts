9ce8904aa72014194fc169e27e8c452270a479f7
JadsonLucena <jadsonlucena@gmail.com>
2022-12-23 12:35:18 -0300

    feat (role repository): delete

--- src/infra/gateways/repository/Role.ts
+++ src/infra/gateways/repository/Role.ts
@@ -375,4 +375,24 @@ export default class RoleRepository extends Repository implements IRoleRepositor
 
 	}
 
+	async delete(id: string, transactionId?: string): Promise<boolean> {
+
+		const [ rows, state, metadata ] = await (transactionId ? this.transactions[transactionId] : this.database).run({
+			sql: `DELETE FROM roles WHERE id = @id`,
+			params: {
+				id: new UUID(id).toString()
+			},
+			types: {
+				id: 'string'
+			}
+		}).catch((err: any) => {
+
+			throw err
+
+		})
+
+		return rows.length > 0
+
+	}
+
 }
