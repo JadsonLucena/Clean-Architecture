a0e614c78e79c3701deee39177b3fd861a4eb8b7
JadsonLucena <jadsonlucena@gmail.com>
2022-12-23 12:48:15 -0300

    feat (member repository): find

--- src/infra/gateways/repository/Member.ts
+++ src/infra/gateways/repository/Member.ts
@@ -29,4 +29,104 @@ export default class MemberRepository extends Repository implements IMemberRepos
 
 	}
 
+	async find({
+		id,
+		projectId,
+		userId,
+		roleId,
+		createdBy,
+		createdAt
+	}: {
+		id?: string,
+		projectId: string,
+		userId?: string,
+		roleId?: string,
+		createdBy?: string,
+		createdAt?: string
+	}, transactionId?: string): Promise<Member[]> {
+
+		let tables = [`members`]
+		let where: string[] = []
+
+		let params: {
+			id?: string,
+			projectId?: string,
+			userId?: string,
+			roleId?: string,
+			createdBy?: string,
+			createdAt?: string
+		} = {}
+		let types: {
+			id?: 'string',
+			projectId?: 'string',
+			userId?: 'string',
+			roleId?: 'string',
+			createdBy?: 'string',
+			createdAt?: 'timestamp'
+		} = {}
+
+		if (id) {
+
+			where.push(`id = @id`)
+			params.id = new UUID(id).toString()
+			types.id = 'string'
+
+		} else {
+
+			if (projectId) {
+
+				where.push(`project_id = @projectId`)
+				params.projectId = new UUID(projectId).toString()
+				types.projectId = 'string'
+
+			}
+
+			if (userId) {
+
+				where.push(`user_id = @userId`)
+				params.userId = new UUID(userId).toString()
+				types.userId = 'string'
+
+			}
+
+			if (roleId) {
+
+				where.push(`role_id = @roleId`)
+				params.roleId = new UUID(roleId).toString()
+				types.roleId = 'string'
+
+			}
+
+			if (createdBy) {
+
+				where.push(`created_by = @createdBy`)
+				params.createdBy = new UUID(createdBy).toString()
+				types.createdBy = 'string'
+
+			}
+
+			if (createdAt) {
+
+				where.push(`created_at = @createdAt`)
+				params.createdAt = new Date(createdAt).toISOString()
+				types.createdAt = 'timestamp'
+
+			}
+
+		}
+
+		const [ rows, state, metadata ] = await (transactionId ? this.transactions[transactionId] : this.database).run({
+			sql: `SELECT * FROM ${tables.join(`, `)} ${where.length ? `WHERE ${where.join(` AND `)}` : ''}`,
+			params,
+			types
+		}).catch((err: any) => {
+
+			throw err
+
+		})
+
+		return rows.map((row: any) => new Member(row.toJSON()))
+
+	}
+
 }
