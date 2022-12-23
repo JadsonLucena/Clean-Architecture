1c24c2f3697c5be668a91d3412e872a15defe408
JadsonLucena <jadsonlucena@gmail.com>
2022-12-23 12:49:01 -0300

    feat (member repository): delete

--- src/infra/gateways/repository/Member.ts
+++ src/infra/gateways/repository/Member.ts
@@ -239,4 +239,24 @@ export default class MemberRepository extends Repository implements IMemberRepos
 
 	}
 
+	async delete(id: string, transactionId?: string): Promise<boolean> {
+
+		const [ rows, state, metadata ] = await (transactionId ? this.transactions[transactionId] : this.database).run({
+			sql: `DELETE FROM members WHERE id = @id`,
+			params: {
+				id: new UUID(id).toString()
+			},
+			types: {
+				id: 'string'
+			}
+		}).catch((err: any) => {
+
+			throw err
+
+		})
+
+		return rows.length > 0
+
+	}
+
 }
